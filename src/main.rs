869334b356a9cb27e60b1c4fd17119a040789688
Seamus Riordan <seamus@riordan.io>
2021-12-08 07:57:20 -0600

    Part 2 solution

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,15 @@ fn main() {
     let inputs = fs::read_to_string("input.txt").unwrap();
 
     let mut counts = 0;
+    let mut sum = 0;
+
     for input in inputs.lines() {
+
         let mut parts = input.split(" | ");
 
         let decoder = find_decoder(parts.next().unwrap());
 
-        for token in parts.next().unwrap().split(" ") {
+        for (i, token) in parts.next().unwrap().split(" ").enumerate() {
             let decoded_value = decode(token, &decoder);
             print!("{:?} ", decoded_value);
             match decoded_value {
@@ -22,11 +25,19 @@ fn main() {
                 _ => {}
             }
 
+            match i {
+                0 => sum += decoded_value * 1000,
+                1 => sum += decoded_value * 100,
+                2 => sum += decoded_value * 10,
+                3 => sum += decoded_value * 1,
+                _ => {}
+            }
         }
         println!();
     }
 
 
     println!("counts {}", counts);
+    println!("sum {}", sum);
 
 }
